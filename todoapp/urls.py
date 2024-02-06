99d61beca5d92775adb1056cabd46168d9c10de4
CloudFlavorKettle <ekaqh8624@gmail.com>
2024-02-06 22:27:46 +0900

    todoapp toggle 및 데이터베이스 연동 구현

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,5 +5,6 @@ from todoapp import views
 app_name = 'todoapp'
 
 urlpatterns = [
-    path('', views.TodoListView.as_view(), name='todo')
+    path('', views.TodoListView.as_view(), name='todo'),
+    path('todo-checking/<int:id>/', views.todo_checking, name='todo_checking'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
 from todoapp.models import Todo
 
+def todo_checking(request, id):
+    try:
+        check = Todo.objects.get(id=id)
+        check.done = not check.done  # Flip the value of 'done'
+        check.save()
+        return HttpResponse("Todo status toggled successfully.")
+    except Todo.DoesNotExist:
+        return HttpResponse("Todo with the provided ID does not exist.")
 
-# Create your views here.
+
 class TodoListView(ListView):
     model = Todo
-    context_object_name = 'todoapp/todo_list.html'
+    context_object_name = 'todo_list'
+    template_name = 'todoapp/todo_list.html'
+
