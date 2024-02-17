96fbee65ba2c1933fffe176e757c9e01e5e1091d
CloudFlavorKettle <ekaqh8624@gmail.com>
2024-02-17 19:13:53 +0900

    accountapp signup add

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,15 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User as DjangoUser
-
-
-# Create your models here.
-class User(DjangoUser):
-
-    def get_full_name(self):
-        return self
-
-    def get_short_name(self):
-        return self.username
-
-    def __str__(self):
-        return self
